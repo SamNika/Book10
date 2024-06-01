01ff61ec89721c03b7e9e12118a9167a5df9f840
nastya675756546 <meshcheryakova-nastya@internet.ru>
2024-06-01 11:52:42 +0300

    файлы 10-й темы практики

---
+++ app/src/main/java/ru/rsue/marushko/bookdepository/BookListActivity.java
@@ -0,0 +1,13 @@
+package ru.rsue.marushko.bookdepository;
+
+
+import androidx.fragment.app.Fragment;
+
+public class BookListActivity extends SingleFragmentActivity{
+    @Override
+    protected Fragment createFragment(){
+        return new BookListFragment();
+    }
+
+}
+
